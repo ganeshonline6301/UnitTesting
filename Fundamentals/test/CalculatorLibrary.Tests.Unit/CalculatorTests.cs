2ee208e007c988afc997e99e0d96c301cc4e4141
Ganesh Sri Surya <robinganesh007@gmail.com>
2025-02-12 00:31:43 +0530

    created a new unittesting project

---
+++ Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorLibrary.Tests.Unit;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldAddTwoNumbers_WhenToNumbersAreIntegers()
+    {
+        var calculator = new Calculator();
+        
+        var result = calculator.Add(5, 2);
+        
+        Assert.Equal(7, result);
+    }
+}
